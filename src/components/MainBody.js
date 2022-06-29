0f82e42db983baac00b34734e1fb4398343d819b
Kitsunebackfire <kwolz92@gmail.com>
2022-06-29 11:48:31 -0500

    setup props to be read properly through MainBody.js

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -7,6 +7,11 @@ const MainBody = ({ weatherData }) => {
   const weatherDescription = weatherData.weather[0].description;
   // had to convert from kelvin and then round via math.round to eliminate excess decimal places
   const temp = Math.round((weatherData.main.temp - 273.15) * (9 / 5) + 32);
+  const feelsLike = Math.round(
+    (weatherData.main.feels_like - 273.15) * (9 / 5) + 32
+  );
+  const windSpeed = weatherData.wind.speed;
+  const humidity = weatherData.main.humidity;
 
   return (
     <main className="mainBody__container">
@@ -21,9 +26,9 @@ const MainBody = ({ weatherData }) => {
           <div className="mainBody__fahrenheit">°F</div>
         </div>
         <div className="mainBody__details">
-          <div className="mainBody__feelsLike">Feels like: 93°F</div>
-          <div className="mainBody__wind">Wind speed: 23 MPH</div>
-          <div className="mainBody__humidity">Humidity: 71%</div>
+          <div className="mainBody__feelsLike">Feels like: {feelsLike}°F</div>
+          <div className="mainBody__wind">Wind speed: {windSpeed} MPH</div>
+          <div className="mainBody__humidity">Humidity: {humidity}%</div>
         </div>
       </div>
     </main>
